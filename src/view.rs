327443da329f271ab1eaba982e218993decd84a2
minish <minish@riseup.net>
2023-01-08 20:10:30 -0500

    revert cache refreshing upon view for now

--- src/view.rs
+++ src/view.rs
@@ -11,10 +11,9 @@ use axum::{
     response::{IntoResponse, Response},
 };
 
-use bytes::{Bytes, BytesMut};
 use hyper::StatusCode;
 use mime_guess::mime;
-use tokio::{fs::File, io::AsyncReadExt};
+use tokio::fs::File;
 use tokio_util::io::ReaderStream;
 
 use crate::cache;
@@ -54,68 +53,6 @@ pub async fn view(
         .unwrap_or_default()
         .to_string();
 
-    let mut cache = state.cache.lock().await;
-
-    let cache_item = cache.get(&name.clone());
-
-    if cache_item.is_none() {
-        let mut path = PathBuf::new();
-        path.push("uploads/");
-        path.push(name.clone());
-
-        if !path.exists() || !path.is_file() {
-            return StatusCode::NOT_FOUND.into_response();
-        }
-
-        let mut file = File::open(path).await.unwrap();
-        let file_len = file.metadata().await.unwrap().len() as usize;
-
-        if file_len < cache::MAX_LENGTH {
-            info!(target: "view", "recaching upload from disk");
-
-            let mut data = BytesMut::zeroed(file_len);
-            file.read_buf(&mut data.as_mut()).await.unwrap();
-            let data = data.freeze();
-
-            cache.insert(name.clone(), data.clone(), Some(cache::DURATION));
-
-            return cache::get_response(&mut cache, original_path);
-        } else {
-            let reader = ReaderStream::new(file);
-            let stream = StreamBody::new(reader);
-
-            info!(target: "view", "reading upload from disk");
-
-            return stream.into_response();
-        }
-    }
-
-    info!(target: "view", "reading upload from cache");
-
-    return cache::get_response(&mut cache, original_path);
-}
-
-/* #[axum::debug_handler]
-pub async fn view(
-    State(state): State<Arc<crate::state::AppState>>,
-    Path(original_path): Path<PathBuf>,
-) -> Response {
-    // (hopefully) prevent path traversal, just check for any non-file components
-    if original_path
-        .components()
-        .into_iter()
-        .any(|x| !matches!(x, Component::Normal(_)))
-    {
-        error!(target: "view", "a request attempted path traversal");
-        return StatusCode::NOT_FOUND.into_response();
-    }
-
-    let name = original_path
-        .file_name()
-        .and_then(OsStr::to_str)
-        .unwrap_or_default()
-        .to_string();
-
     let cache = state.cache.lock().await;
 
     let cache_item = cache.get(&name);
@@ -130,10 +67,6 @@ pub async fn view(
         }
 
         let file = File::open(path).await.unwrap();
-
-        if file.metadata().await.unwrap().len() < (cache::MAX_LENGTH as u64) {
-            info!("file can be cached");
-        }
 
         let reader = ReaderStream::new(file);
         let stream = StreamBody::new(reader);
@@ -162,4 +95,4 @@ pub async fn view(
     );
 
     return res;
-} */
+}
